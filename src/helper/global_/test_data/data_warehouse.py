2c45acf15ea9b91e373db9ce916478a95325f1d2
tober <a.svarych@orderry.com>
2020-02-08 22:50:05 +0200

    prepare posting data #2

--- src/helper/global_/test_data/data_warehouse.py
+++ src/helper/global_/test_data/data_warehouse.py
@@ -1,7 +1,7 @@
 from src.helper.global_.randomizer import *
 
 
-def get_random_goods_data(serial=False):
+def get_random_items_data(serial=False):
     random_suffix = get_random_low_string(5, with_digits=True)
     quantity = get_random_int(10, 20)
     data = {
@@ -21,4 +21,10 @@ def get_random_posting_data():
         'supplier': f'{get_random_first_name()} {get_random_last_name()}',
         'stock': get_random_stock_name()
     }
+    return data
+
+
+def get_random_goods_data(serial=False):
+    data = get_random_posting_data()
+    data.update(get_random_items_data(serial))
     return data
